b1df1117abf39e86fdb1b77cf9ad688298da854f
Agrim <agrimsharma1714@gmail.com>
2022-08-26 23:45:59 +0530

    few optimization

--- javascript_clock.js
+++ javascript_clock.js
@@ -1,8 +1,10 @@
 setInterval(() => {
     d = new Date();
+    
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
+
     hRotation = 30*htime + mtime/2;
     mRotation = 6*mtime;
     sRotation = 6*stime;
