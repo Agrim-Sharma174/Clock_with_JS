d5bd2a46e61ecaa996af2dd47e97ba1b2bde8783
Agrim <agrimsharma1714@gmail.com>
2022-10-08 23:47:33 +0530

    optimized little bit

--- javascript_clock.js
+++ javascript_clock.js
@@ -1,15 +1,15 @@
 setInterval(() => {
-    d = new Date();
-    
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
+  d = new Date();
 
-    hRotation = 30*htime + mtime/2;
-    mRotation = 6*mtime;
-    sRotation = 6*stime;
+  htime = d.getHours();
+  mtime = d.getMinutes();
+  stime = d.getSeconds();
 
-    hour.style.transform = `rotate(${hRotation}deg)`
-    minute.style.transform = `rotate(${mRotation}deg)`
-    second.style.transform = `rotate(${sRotation}deg)`
+  hRotation = 30 * htime + mtime / 2;
+  mRotation = 6 * mtime;
+  sRotation = 6 * stime;
+
+  hour.style.transform = `rotate(${hRotation}deg)`;
+  minute.style.transform = `rotate(${mRotation}deg)`;
+  second.style.transform = `rotate(${sRotation}deg)`;
 }, 1000);
